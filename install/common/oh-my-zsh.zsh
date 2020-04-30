fa7353625ce2a89809946737ce8c50a0751cb074
David Ker <snappy316@gmail.com>
2020-04-30 09:17:00 -0700

    Fix typo in p10k clone command

--- install/common/oh-my-zsh.zsh
+++ install/common/oh-my-zsh.zsh
@@ -4,7 +4,7 @@ if [ ! -d ~/.oh-my-zsh ] ; then
   echo "Installing Oh My Zsh"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/themes/powerlevel10k
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
   git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
