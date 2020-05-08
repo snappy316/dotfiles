a8105a99558ca48e405dde89802ad1a03d487f90
David Ker <snappy316@gmail.com>
2020-05-08 13:57:30 -0700

    Add tpm installation to tmux install script
    H/T https://github.com/tmux-plugins/tpm/issues/6#issuecomment-622178474

--- install/common/tmux.zsh
+++ install/common/tmux.zsh
@@ -4,3 +4,6 @@ if [ ! -d ~/.tmux/plugins/tpm ] ; then
   echo "Installing tpm"
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
+
+echo "Updating tmux plugins"
+~/.tmux/plugins/tpm/bin/install_plugins
