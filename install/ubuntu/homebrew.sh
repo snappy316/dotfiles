01e317c30b0f047048cff2309fe5917143217f64
David Ker <snappy316@gmail.com>
2021-05-06 22:21:40 +0000

    Fixes to initial Ubuntu install
    - Ensure a `zshrc.local` file exists, otherwise ZSH complains
    - The `install/linux` script didn't like not having `/install/` before
      all the sub-scripts for some reason :shrugs:
    - Tweak the Homebrew/Linuxbrew installation to make sure it runs:
      - Regardless of the user account it's installed under
      - Regardless of whether it was installed via the `sudo` path or not

--- install/ubuntu/homebrew.sh
+++ install/ubuntu/homebrew.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install build-essential
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/david/.zprofile
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+echo 'eval $(~/.linuxbrew/bin/brew shellenv)' >> ~/.zprofile
+echo '# Add linuxbrew to path' >> ~/.dotfiles/zsh/zshrc.local
+echo 'path+=("$HOME/.linuxbrew/bin")' >> ~/.dotfiles/zsh/zshrc.local
+eval $(~/.linuxbrew/bin/brew shellenv)
