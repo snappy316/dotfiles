ae80537eecb52373818cabda7d642d28adb2aa8d
David Ker <snappy316@gmail.com>
2021-05-06 22:46:55 +0000

    Change dotfiles location to GitHub

--- install/common/dotfiles.sh
+++ install/common/dotfiles.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d ~/.dotfiles ] ; then
   echo "Downloading dotfiles"
-  git clone https://gitlab.com/snappy316/dotfiles.git ~/.dotfiles
+  git clone git@github.com:snappy316/dotfiles.git ~/.dotfiles
 else
   echo "Updating dotfiles"
   cd ~/.dotfiles
