07be44fbc7ae98605921893ab0a9b0eb9aab878b
David Ker <snappy316@gmail.com>
2021-08-24 23:17:01 +0000

    Gracefully handle SSH Agent Forwarding
    Following the recommendations in the article below, this seems to get
    SSH Agent Forwarding playing nicely with tmux.
    
    A few notes:
    - After installing this, disconnect and reconnect to the ssh session so
    that the `~/.ssh/rc` file is run
    - You may either need to reload the `.tmux.conf` file (prefix-r), or
    restart the tmux server `tmux kill-server` to get the config changes
    
    H/T https://blog.testdouble.com/posts/2016-11-18-reconciling-tmux-and-ssh-agent-forwarding/

--- install/common/tmux.zsh
+++ install/common/tmux.zsh
@@ -7,3 +7,11 @@ fi
 
 echo "Updating tmux plugins"
 ~/.tmux/plugins/tpm/bin/install_plugins
+
+# Gracefully handle SSH Agent Forwarding
+if read -q "choice?Is this a remote machine you'll be SSHing to?? (y/N): "; then
+  echo
+  echo "Setting up tmux / SSH Agent Forwarding"
+  cp ~/.dotfiles/ssh/rc ~/.ssh/rc
+  cat ~/.dotfiles/ssh/tmux.conf.local >> ~/.dotfiles/.tmux.conf.local
+fi
