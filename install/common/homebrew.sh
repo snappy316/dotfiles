2d801ffdcd43af99a01cfe8ea9809311606e105e
David Ker <snappy316@gmail.com>
2022-01-03 19:28:03 +0000

    Update Ubuntu Homebrew installation
    When I originally installed this on my Ubuntu DO Droplet, it installed
    Homebrew into my homedir (`~/.linuxbrew`), instead of a common location
    (`/home/linuxbrew/.linuxbrew`)...meaning when I tried to add new stuff,
    it would complain, and sometimes just not work.
    
    I uninstalled/reinstalled to the correct location, and took the
    opportunity to update the scripts accordingly as well.

--- install/common/homebrew.sh
+++ install/common/homebrew.sh
@@ -3,7 +3,7 @@
 which brew
 if [[ $? != 0 ]] ; then
   echo "Installing Homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "Installing Brewfile..."
   brew bundle --no-upgrade
 else
