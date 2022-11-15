c064910f846867e6f4a0e0e81e01d69f6d8af762
David Ker <snappy316@gmail.com>
2022-11-15 19:31:20 +0000

    Ensure destination directory exists before cloning Vundle

--- install/common/vim.sh
+++ install/common/vim.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ] ; then
   echo "Installing Vundle"
+  mkdir -p ~/.vim/bundle
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
