8b0f0451f596175221d296daff812864f72daac9
David Ker <snappy316@gmail.com>
2024-01-13 16:57:48 -0800

    Redo all the nvim linting and formatting for eslint and prettier
    https://www.josean.com/posts/neovim-linting-and-formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,41 +1,34 @@
 return {
   "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   opts = {
     formatters_by_ft = {
-      -- Use a sub-list to run only the first available formatter
-      cpp = { "clang_format" },
-      javascript = { { "eslint_d", "prettier" } },
-      javascriptreact = { { "eslint_d", "prettier" } },
-      typescript = { { "eslint_d", "prettier" } },
-      typescriptreact = { { "eslint_d", "prettier" } },
-      ruby = { "stree" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      -- svelte = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      -- graphql = { "prettier" },
+      lua = { "stylua" },
+      -- python = { "isort", "black" },
     },
-    formatters = {
-      --injected = { options = { ignore_errors = true } },
-      eslint_d = {
-        condition = function(ctx)
-          local b = vim.b[vim.api.nvim_get_current_buf()]
-          if b.eslint_has_prettier ~= nil then
-            return b.eslint_has_prettier
-          end
-
-          b.eslint_has_prettier = false
-          local path =
-            vim.fs.find({ ".eslintrc", ".eslintrc.js", ".eslintrc.json" }, { path = ctx.filename, upward = true })[1]
-          if path then
-            local file = io.open(path, "r")
-            if file then
-              local content = file:read("*a") -- *a or *all reads the whole file
-              if string.find(content, "prettier/prettier") then
-                b.eslint_has_prettier = true
-              end
-              file:close()
-            end
-          end
-
-          return b.eslint_has_prettier
-        end,
-      },
-    },
+    -- format_on_save = {
+    --   lsp_fallback = true,
+    --   async = false,
+    --   timeout_ms = 500,
+    -- },
   },
+
+  -- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+  --   conform.format({
+  --     lsp_fallback = true,
+  --     async = false,
+  --     timeout_ms = 1000,
+  --   })
+  -- end, { desc = "Format file or range (in visual mode)" })
 }
